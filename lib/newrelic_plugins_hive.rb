11512cb1d081073e60676cd346df44545dc994fc
Elad Maimon <elad.maimon@klarna.com>
2013-12-19 11:39:35 +0200

    downloader now downlaods and extracts plugins

--- lib/newrelic_plugins_hive.rb
+++ lib/newrelic_plugins_hive.rb
@@ -1,6 +1,21 @@
 require 'newrelic_plugins_hive/version'
 require 'newrelic_plugins_hive/configuration'
+require 'newrelic_plugins_hive/downloader'
 require 'newrelic_plugins_hive/bypass_setup_and_run'
 
 module NewRelicPluginsHive
+  
+  class << self
+    def config
+      @configuration ||= Configuration.new
+    end
+    
+    def install
+      downloader = Downloader.new
+      
+      config.plugins.each do |name, options|
+        downloader.install_plugin(name, options)
+      end
+    end
+  end
 end
---
+++ lib/newrelic_plugins_hive/downloader.rb
@@ -0,0 +1,41 @@
+module NewRelicPluginsHive
+
+  class Downloader
+    def initialize
+      validate_wget_exists
+      `mkdir -p temp`
+      `mkdir -p plugins`
+    end
+    
+    def install_plugin(name, options)
+      require 'pry'
+      binding.pry
+      puts "Installing plugin #{name}..."
+      
+      validate_plugin_source
+      download_and_extract(name, options)
+    end
+    
+    private
+    
+    def validate_wget_exists
+      raise "Wget must be installed on your machine. For more help: http://www.gnu.org/software/wget/" unless `wget -V` =~ /^GNU Wget/
+    end
+
+    def validate_plugin_source
+      raise "\tMust specify github source for plugin #{name}" unless options[:github] =~ /^https:\/\/github.com/
+    end
+
+    def download_and_extract(name, options)
+      `wget -O temp/#{name}.zip #{options[:github]}/archive/master.zip`
+
+      files_in_zip = `zipinfo -1 temp/#{name}.zip`.split("\n")
+      full_agent_path = files_in_zip.find_all {|s| s =~ /#{options[:agent_path]}$/}
+      
+      raise "\tAgent path must exists and be unique in plugin" unless full_agent_path.size == 1
+
+      `unzip -p temp/#{name}.zip "#{full_agent_path.first}" > plugins/#{options[:agent_path]}`
+    end
+  end
+  
+end
