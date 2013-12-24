159abc948cf0b6adc22ea4ce0ea3e78ebf863e04
Elad Maimon <elad.maimon@klarna.com>
2013-12-24 19:35:43 +0200

    adding test to RunWrapper.call_original_run

--- spec/newrelic_plugins_hive/run_wrapper_spec.rb
+++ spec/newrelic_plugins_hive/run_wrapper_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe 'RunWrapper' do
-  before { NewRelicPluginsHive::RunWrapper.bypass_run }
 
-  subject { ::NewRelic::Plugin::Run }
+  describe '.bypass_run' do
+    before { NewRelicPluginsHive::RunWrapper.bypass_run }
 
-  its(:setup_and_run) { should == 'Bypassing setup_and_run'}
-  it { should respond_to :orig_setup_and_run }
-  it { expect { subject.orig_setup_and_run }.to raise_error Errno::ENOENT }
+    subject { ::NewRelic::Plugin::Run }
+
+    its(:setup_and_run) { should == 'Bypassing setup_and_run'}
+    it { should respond_to :orig_setup_and_run }
+    it { expect { subject.orig_setup_and_run }.to raise_error Errno::ENOENT }
+  end
+  
+  describe '.call_original_run' do
+    it 'should call original setup_and_run' do
+      ::NewRelic::Plugin::Run.should_receive(:orig_setup_and_run)
+      NewRelicPluginsHive::RunWrapper.call_original_run
+    end
+  end
 end
